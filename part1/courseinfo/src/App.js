fff16255ad7880b0ea0ad14a00d5eaf6c586cd49
lwszolek <lwszolek@gmail.com>
2022-09-06 11:15:41 -0500

    finished 1.4 - missed the line where it said 'no need for loops

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,39 +7,36 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-    return (
-        <div>
-            <Part part = {props.part1} />
-            <Part part = {props.part2} />
-            <Part part = {props.part3} />
-        </div>
-    )
+    const parts = []
+    return <div>{props.parts.map(part => <Part part = {part} />)}</div>
 }
 
 const Total = (props) => {
-    return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    return <p>Number of exercises {props.parts.reduce( (acc, curr) => acc + curr.exercises, 0 )}</p> 
 }
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [ 
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
     return (
         <div>
             <Header course = {course} />
-            <Content part1 = {part1} part2 = {part2} part3 = {part3} />
-            <Total exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises} />
+            <Content parts = {parts} />
+            <Total parts = {parts} /> 
         </div>
     )
 }
