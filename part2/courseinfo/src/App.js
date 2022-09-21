7d9b36a5e847bb57973a7c818720aa648f3ac21e
lwszolek <lwszolek@gmail.com>
2022-09-21 18:04:36 -0500

    completed 2.5 and added a missed key for Course

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,53 +1,4 @@
-const Header = ({ name }) => {
-    return (
-        <div>
-            <h1>{name}</h1>
-        </div>
-    )
-}
-
-const Content = ({ parts }) => {
-    return(
-        <div>
-            <ul>
-                {parts.map(part => 
-                    <Part key={part.id} part={part} />
-                )}
-            </ul>
-        </div>
-    )
-}
-
-const Part = ({ part }) => {
-    return (
-        <li key = {part.id}>
-            {part.name} {part.exercises}
-        </li>
-    )
-}
-
-const Course = ({ course }) =>{
-    return (
-        <div>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    )
-}
-
-const Total = ({ parts }) => {
-    return(
-        <div> 
-            <b>
-                total of {parts.reduce(
-                    (prev, curr) => prev + curr.exercises,
-                    0
-                )} exercises
-            </b>
-        </div>
-    )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
@@ -99,7 +50,7 @@ const App = () => {
   return (
       <div>
           {courses.map( course =>
-            <Course course={course} />
+            <Course key={course.id} course={course} />
         )}
       </div>
   )
