f2907c56c173d9196149c4cd4bd5eb8378ab49ef
bambery <lwszolek@gmail.com>
2022-10-19 15:46:50 -0500

    Completed 5.2 - Blog FE: remember logged in users/allow logout

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -15,6 +15,15 @@ const App = () => {
         )
     }, [])
 
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+        if(loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+            blogService.setToken(user.token)
+        }
+    }, [])
+
     const handleLogin = async (event) => {
         event.preventDefault()
 
@@ -36,8 +45,14 @@ const App = () => {
         }
     }
 
+    const handleLogout = () => {
+        window.localStorage.removeItem('loggedBlogappUser')
+        setUser(null)
+    }
+
     const loginForm = () => (
         <form onSubmit={ handleLogin }>
+            <h2>log in to application</h2>
             <div>
                 username
                 <input
@@ -62,9 +77,10 @@ const App = () => {
 
     const blogList = () => (
         <div>
+            <button onClick={handleLogout}>logout</button>
             <h2>blogs</h2>
             {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
+                <Blog key={blog.id} blog={blog} />
             )}
         </div>
     )
